fab75ebfb977a9fa954b6e46d1f2cc30685dfebb
Slavomir Bavala <slavomir.bavala@hotovo.org>
2019-03-26 23:49:07 +0100

    app now listens on "/" path

--- src/main/java/org/hotovo/Main.java
+++ src/main/java/org/hotovo/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-            server.createContext("/test", new SlackRequestHandler());
+            server.createContext("/", new SlackRequestHandler());
             server.setExecutor(null); // creates a default executor
             server.start();
             System.out.println("Server is listening on port 8080");
